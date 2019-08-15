3445a6e1f8ba607a49f576a124a48619054ec2a3
Anand Desai <31744791+desaianand1@users.noreply.github.com>
2019-08-15 02:12:09 -0400

    Added custom stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import lk.vivoxalabs.customstage.CustomStage;
+import lk.vivoxalabs.customstage.CustomStageBuilder;
+import lk.vivoxalabs.customstage.tools.HorizontalPos;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
-    private Stage primaryStage;
     private static final String mainFXML = "view/main.fxml";
+    private static final String cssURL = "css/";
+    private static final String imgURL = "images/";
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -17,16 +22,34 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Parent root = null;
-        this.primaryStage = primaryStage;
-
+        CustomStage stage = null;
         try {
             root = FXMLLoader.load(getClass().getResource(mainFXML));
+
+            stage = new CustomStageBuilder()
+                    .setWindowTitle("Callisto", HorizontalPos.RIGHT, HorizontalPos.CENTER)
+                    .setActionIcons(
+                            new Image(imgURL + "window-close.svg"),    //Close
+                            new Image(imgURL + "window-minimize.svg"), //Minimize
+                            new Image(imgURL + "window-maximize.svg"), // Maximize
+                            new Image(imgURL + "window-restore.svg"))  // Restore
+                    .setTitleColor("black")
+//                    .setStyleSheet(getClass().getResource(cssURL + "title_bar.css"))
+                    .setIcon(imgURL + "logo.png")
+                    .setButtonHoverColor("FFAB40", "FFAB40", "d32f2f")
+                    .setDimensions(450, 450, 1920, 1080) //min,max values for window resizing
+                    .setWindowColor("FF6D00")
+                    .build();
+
         } catch (IOException e) {
+            e.getMessage();
             e.printStackTrace();
         }
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        if (root != null && stage != null) {
+//            Scene scene = new Scene(root);
+//            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
---
+++ src/main/java/controllers/Controller.java
@@ -0,0 +1,19 @@
+package controllers;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    /**
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
